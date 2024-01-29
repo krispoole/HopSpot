6ba2711bfd0a675230d70cfae6980341d09bdc7f
Kris Poole <krispoole@pm.me>
2024-01-29 09:56:09 -0600

    initial commit

---
+++ hopspot-api/src/main/java/com/hopspot/hopspotapi/HopSpotApiApplication.java
@@ -0,0 +1,13 @@
+package com.hopspot.hopspotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopSpotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopSpotApiApplication.class, args);
+	}
+
+}
---
+++ hopspot-api/src/test/java/com/hopspot/hopspotapi/HopSpotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopspot.hopspotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopSpotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
